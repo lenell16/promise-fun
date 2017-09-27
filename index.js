e232cff2970d1ddac77460144cbc9c19e91b93f0
Alonzo Thomas <lenell16@gmail.com>
2017-09-26 20:03:32 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,101 @@
+const delay = require('delay');
+const hardRejection = require('hard-rejection');
+const loudRejection = require('loud-rejection');
+const pAll = require('p-all');
+const pAny = require('p-any');
+const pBreak = require('p-break');
+const pCancelable = require('p-cancelable');
+const pCatchIf = require('p-catch-if');
+const pDebounce = require('p-debounce');
+const pDefer = require('p-defer');
+const pDoWhilst = require('p-do-whilst');
+const pEachSeries = require('p-each-series');
+const pEvent = require('p-event');
+const pEvery = require('p-every');
+const pFilter = require('p-filter');
+const pFinally = require('p-finally');
+const pForever = require('p-forever');
+const pIf = require('p-if');
+const pImmediate = require('p-immediate');
+const pIsPromise = require('p-is-promise');
+const pLazy = require('p-lazy');
+const pLimit = require('p-limit');
+const pLocate = require('p-locate');
+const pLog = require('p-log');
+const pMap = require('p-map');
+const pMapSeries = require('p-map-series');
+const pMemoize = require('p-memoize');
+const pMinDelay = require('p-min-delay');
+const pOne = require('p-one');
+const pPipe = require('p-pipe');
+const pProps = require('p-props');
+const pQueue = require('p-queue');
+const pRace = require('p-race');
+const pReduce = require('p-reduce');
+const pReflect = require('p-reflect');
+const pRetry = require('p-retry');
+const pSeries = require('p-series');
+const pSettle = require('p-settle');
+const pSome = require('p-some');
+const pTap = require('p-tap');
+const pThrottle = require('p-throttle');
+const pTime = require('p-time');
+const pTimeout = require('p-timeout');
+const pTimes = require('p-times');
+const pTry = require('p-try');
+const pWaitFor = require('p-wait-for');
+const pWaterfall = require('p-waterfall');
+const pWhilst = require('p-whilst');
+const pify = require('pify');
+
+module.exports = {
+  delay,
+  hardRejection,
+  loudRejection,
+  pAll,
+  pAny,
+  pBreak,
+  pCancelable,
+  pCatchIf,
+  pDebounce,
+  pDefer,
+  pDoWhilst,
+  pEachSeries,
+  pEvent,
+  pEvery,
+  pFilter,
+  pFinally,
+  pForever,
+  pIf,
+  pImmediate,
+  pIsPromise,
+  pLazy,
+  pLimit,
+  pLocate,
+  pLog,
+  pMap,
+  pMapSeries,
+  pMemoize,
+  pMinDelay,
+  pOne,
+  pPipe,
+  pProps,
+  pQueue,
+  pRace,
+  pReduce,
+  pReflect,
+  pRetry,
+  pSeries,
+  pSettle,
+  pSome,
+  pTap,
+  pThrottle,
+  pTime,
+  pTimeout,
+  pTimes,
+  pTry,
+  pWaitFor,
+  pWaterfall,
+  pWhilst,
+  pify
+};
